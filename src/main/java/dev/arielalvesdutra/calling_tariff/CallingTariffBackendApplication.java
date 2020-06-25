9c1f3138f27dd6e16fae8d6ecf55e1613499f5a4
Ariel Alves Dutra <alves.lsn@gmail.com>
2020-06-25 18:20:30 -0300

    First commit

---
+++ src/main/java/dev/arielalvesdutra/calling_tariff/CallingTariffBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.arielalvesdutra.calling_tariff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingTariffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingTariffBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arielalvesdutra/calling_tariff/CallingTariffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arielalvesdutra.calling_tariff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallingTariffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
